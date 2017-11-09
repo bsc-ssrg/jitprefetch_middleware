ab47dd8aa665f76b262f78fe072e02ad4b9ca41e
Marcsiq2 <marcsiquierpenyafort@gmail.com>
2017-11-09 11:03:00 +0100

    Added imports

--- bsc/jitprefetch.py
+++ bsc/jitprefetch.py
@@ -1,11 +1,52 @@
+import os
+import sys
+import time
+import hashlib
+import urllib2
 import StringIO
-
+import itertools
+import threading
+import multiprocessing
+import cPickle as pickle
+import multiprocessing.pool
+from utils import Singleton
+from itertools import chain
 from swift.common import wsgi
+from datetime import datetime as dt
 from swift.common.swob import wsgify
+from swift.common.swob import Response
+from sys import argv, getsizeof, stderr
 from swift.common.utils import split_path
+from collections import deque, OrderedDict
+from swift.common.internal_client import InternalClient
+from swiftclient.service import SwiftService, SwiftError
+
+
+################### CONFIGURATION ###################
+TOTALSECONDS = 60 #allowed time diff in seconds between previous and next object
+PROB_THRESHOLD = 0.5 #minimum probability to be prefetched
+N_THREADS = 5 #number of threads in the download threadpool
+AUTOSAVE = 30 #autosave chain each X seconds
+MAX_PREFETCHED_SIZE =  1073741824 #1Gb of prefetched objects
+PREFETCH = True #Enables prefetching objects. If False it just updates the chain
+DELETE_WHEN_SERVED = True #True if objects are deleted from memory after being served
+CHAINSAVE = '/tmp/chain.p' #where to save the chain
+WAIT_TIME_MULTIPLIER = 0.5 #wait time for download multiplier
+PROXY_PATH = '/etc/swift/proxy-server.conf' #proxy configuration file
+MAX_TIME_IN_MEMORY = 30 #max seconds for an object to be in memory without being downloaded
+
+
+#Global Variables
+acc_status= [200]
+multiplier = 0.5
+prefetched_objects = OrderedDict()
+
 
 
 class JITPrefetchMiddleware(object):
+
+    __metaclass__ = Singleton
+
     def __init__(self, app, *args, **kwargs):
         self.app = app
       
@@ -15,8 +56,6 @@ class JITPrefetchMiddleware(object):
             (version, account, container, objname) = split_path(request.path_info, 4, 4, True)
         except ValueError:
             return self.app
-
-        preview_path = '/%s/%s/%s/%s' % (version, account, container, objname)
 
         if request.method == 'GET':
             print "hola mundo"
